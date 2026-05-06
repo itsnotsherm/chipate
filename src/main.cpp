2f7be1883db819f58d3e352245fdec4c71dfdb21
Sherman <e1115290@u.nus.edu>
2026-05-06 13:56:12 +0800

    Add template files

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <SDL3/SDL.h>
+#include <iostream>
+
+int main(int, char**) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("chipate", 1024, 512, 0);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) running = false;
+            if (event.type == SDL_EVENT_KEY_DOWN &&
+                event.key.key == SDLK_ESCAPE) running = false;
+        }
+        SDL_Delay(16);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
